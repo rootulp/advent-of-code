53c0317e097f9418608a5b19a5b982e10470c168
Rootul Patel <rootulp@amazon.com>
2020-12-17 22:09:55 -0500

    Refactor isValid helpers to operate on Passport

--- 2020/day4/day4.go
+++ 2020/day4/day4.go
@@ -10,102 +10,6 @@ import (
 	"strings"
 )
 
-type Passport struct {
-	byr int
-	iyr int
-	eyr int
-	hgt string
-	hcl string
-	ecl string
-	pid string
-}
-
-func (passport Passport) isValid() bool {
-	return isValidBirthYear(passport.byr) && isValidIssueYear(passport.iyr) && isValidExpirationYear(passport.eyr) && isValidHeight(passport.hgt) && isValidHairColor(passport.hcl) && isValidEyeColor(passport.ecl) && isValidPassportID(passport.pid)
-}
-
-func isValidBirthYear(birthYear int) bool {
-	return birthYear >= 1920 && birthYear <= 2002
-}
-
-func isValidIssueYear(issueYear int) bool {
-	return issueYear >= 2010 && issueYear <= 2020
-}
-
-func isValidExpirationYear(expirationYear int) bool {
-	return expirationYear >= 2020 && expirationYear <= 2030
-}
-
-func isValidHeight(height string) bool {
-	if len(height) < 2 {
-		fmt.Printf("Height %v is not long enough to contain a unit", height)
-		return false
-	}
-	unit := string(height[len(height)-2:])
-	value, err := strconv.Atoi(string(height[:len(height)-2]))
-	if err != nil {
-		fmt.Printf("Failed to parse height %v\n", err)
-		return false
-	}
-
-	if unit == "cm" {
-		return isValidHeightInCentimeters(value)
-	} else if unit == "in" {
-		return isValidHeightInInches(value)
-	} else {
-		fmt.Println("Unit is not cm or in")
-		return false
-	}
-}
-
-func isValidHeightInInches(inches int) bool {
-	return inches >= 59 && inches <= 76
-}
-
-func isValidHeightInCentimeters(centimeters int) bool {
-	return centimeters >= 150 && centimeters <= 193
-}
-
-func isValidHairColor(hairColor string) bool {
-	if len(hairColor) < 7 {
-		fmt.Printf("hairColor %v is not long enough to be valid\n", hairColor)
-		return false
-	}
-
-	if !strings.HasPrefix(hairColor, "#") {
-		fmt.Printf("hairColor %v does not start with #\n", hairColor)
-		return false
-	}
-	hexadecimalColor := strings.TrimPrefix(hairColor, "#")
-	_, err := hex.DecodeString(hexadecimalColor)
-	if err != nil {
-		fmt.Printf("Failed to decode hexadecimalColor %v\n", hexadecimalColor)
-		return false
-	}
-	return true
-
-}
-
-func isValidEyeColor(eyeColor string) bool {
-	for _, color := range getValidEyeColors() {
-		if color == eyeColor {
-			return true
-		}
-	}
-	fmt.Printf("Invalid eye color %s\n", eyeColor)
-	return false
-}
-
-func getValidEyeColors() []string {
-	return []string{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
-}
-
-func isValidPassportID(passportID string) bool {
-	length := len(passportID)
-	fmt.Printf("PassportId length %v", length)
-	return length == 9
-}
-
 func main() {
 	lines := readFile("./input.txt")
 
@@ -124,7 +28,7 @@ func IsValidPassport(line string) (isValid bool) {
 	if err != nil {
 		return false
 	}
-	return passport.isValid()
+	return passport.IsValid()
 }
 
 func getProperties(line string) (properties map[string]string) {
@@ -144,34 +48,35 @@ func getProperties(line string) (properties map[string]string) {
 // getPassport returns a passport struct for the provided line or returns err if
 // the provided line could not be parsed into a passport.
 func getPassport(line string) (passport Passport, err error) {
+
 	passport = Passport{}
 
 	properties := getProperties(line)
 
 	// Set all string types of passport
-	passport.hgt = properties["hgt"]
-	passport.hcl = properties["hcl"]
-	passport.ecl = properties["ecl"]
-	passport.pid = properties["pid"]
+	passport.Height = properties["hgt"]
+	passport.HairColor = properties["hcl"]
+	passport.EyeColor = properties["ecl"]
+	passport.PassportId = properties["pid"]
 
 	// Parse strings into ints
 	byr, err := strconv.Atoi(properties["byr"])
 	if err != nil {
 		return passport, err
 	}
-	passport.byr = byr
+	passport.BirthYear = byr
 
 	iyr, err := strconv.Atoi(properties["iyr"])
 	if err != nil {
 		return passport, err
 	}
-	passport.iyr = iyr
+	passport.IssueYear = iyr
 
 	eyr, err := strconv.Atoi(properties["eyr"])
 	if err != nil {
 		return passport, err
 	}
-	passport.eyr = eyr
+	passport.ExpirationYear = eyr
 
 	fmt.Printf("Passport %#v\n", passport)
 	return passport, nil
@@ -201,4 +106,103 @@ func readFile(filename string) (potentialPassports []string) {
 	potentialPassports = append(potentialPassports, strings.Join(partialPassport, " "))
 	// fmt.Printf("passportLines %#v\n", potentialPassports)
 	return
+}
+
+// Passport represents a passport. All fields in this struct are required for
+// valid passports.
+type Passport struct {
+	BirthYear      int
+	IssueYear      int
+	ExpirationYear int
+	Height         string
+	HairColor      string
+	EyeColor       string
+	PassportId     string
+}
+
+// IsValid returns whethere the passport is a valid passport.
+func (passport Passport) IsValid() bool {
+	return passport.isValidBirthYear() && passport.isValidIssueYear() && passport.isValidExpirationYear() && passport.isValidHeight() && passport.isValidHairColor() && passport.isValidEyeColor() && passport.isValidPassportID()
+}
+
+func (passport Passport) isValidBirthYear() bool {
+	return passport.BirthYear >= 1920 && passport.BirthYear <= 2002
+}
+
+func (passport Passport) isValidIssueYear() bool {
+	return passport.IssueYear >= 2010 && passport.IssueYear <= 2020
+}
+
+func (passport Passport) isValidExpirationYear() bool {
+	return passport.ExpirationYear >= 2020 && passport.ExpirationYear <= 2030
+}
+
+func (passport Passport) isValidHeight() bool {
+	if len(passport.Height) < 2 {
+		fmt.Printf("Height %v is not long enough to contain a unit", passport.Height)
+		return false
+	}
+	unit := string(passport.Height[len(passport.Height)-2:])
+	value, err := strconv.Atoi(string(passport.Height[:len(passport.Height)-2]))
+	if err != nil {
+		fmt.Printf("Failed to parse height %v\n", err)
+		return false
+	}
+
+	if unit == "cm" {
+		return isValidHeightInCentimeters(value)
+	} else if unit == "in" {
+		return isValidHeightInInches(value)
+	} else {
+		fmt.Println("Unit is not cm or in")
+		return false
+	}
+}
+
+func isValidHeightInInches(inches int) bool {
+	return inches >= 59 && inches <= 76
+}
+
+func isValidHeightInCentimeters(centimeters int) bool {
+	return centimeters >= 150 && centimeters <= 193
+}
+
+func (passport Passport) isValidHairColor() bool {
+	if len(passport.HairColor) < 7 {
+		fmt.Printf("hairColor %v is not long enough to be valid\n", passport.HairColor)
+		return false
+	}
+
+	if !strings.HasPrefix(passport.HairColor, "#") {
+		fmt.Printf("hairColor %v does not start with #\n", passport.HairColor)
+		return false
+	}
+	hexadecimalColor := strings.TrimPrefix(passport.HairColor, "#")
+	_, err := hex.DecodeString(hexadecimalColor)
+	if err != nil {
+		fmt.Printf("Failed to decode hexadecimalColor %v\n", hexadecimalColor)
+		return false
+	}
+	return true
+
+}
+
+func (passport Passport) isValidEyeColor() bool {
+	for _, color := range getValidEyeColors() {
+		if color == passport.EyeColor {
+			return true
+		}
+	}
+	fmt.Printf("Invalid eye color %s\n", passport.EyeColor)
+	return false
+}
+
+func getValidEyeColors() []string {
+	return []string{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
+}
+
+func (passport Passport) isValidPassportID() bool {
+	length := len(passport.PassportId)
+	fmt.Printf("PassportId length %v", length)
+	return length == 9
 }
