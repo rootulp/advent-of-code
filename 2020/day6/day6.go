9352a5e853ab7020207d2bf56cc78bf075087e76
Rootul Patel <rootulp@amazon.com>
2020-12-18 21:13:44 -0500

    Solve day6 part2

--- 2020/day6/day6.go
+++ 2020/day6/day6.go
@@ -8,14 +8,50 @@ import (
 )
 
 func main() {
-	responses := ReadFile("input.txt")
+	// Part one
+	responses := ReadFileIntoResponses("input.txt")
+	PrintSumOfUnique(responses)
 
+	// Part two
+	groups := ReadFileIntoGroups("input.txt")
+	PrintSumOfCommon(groups)
+}
+
+// PrintSumOfUnique prints the sum of unique characters in each response
+func PrintSumOfUnique(responses []string) {
 	sum := 0
 	for _, response := range responses {
 		unique := GetNumUnique(response)
 		sum += unique
 	}
 	fmt.Printf("Sum of unique counts %v\n", sum)
+}
+
+// PrintSumOfCommon prints the sum of common responses in each group
+func PrintSumOfCommon(groups [][]string) {
+	sum := 0
+	for _, response := range groups {
+		unique := GetNumCommon(response)
+		sum += unique
+	}
+	fmt.Printf("Sum of common counts %v\n", sum)
+}
+
+// GetNumCommon returns the number of responses that the group has in common
+func GetNumCommon(group []string) (numCommon int) {
+	countOfResponses := make(map[rune]int)
+	for _, response := range group {
+		for _, answer := range response {
+			countOfResponses[answer]++
+		}
+	}
+	numCommon = 0
+	for response := range countOfResponses {
+		if countOfResponses[response] == len(group) {
+			numCommon++
+		}
+	}
+	return numCommon
 }
 
 // GetNumUnique returns the number of unique characters in the provided response
@@ -27,8 +63,8 @@ func GetNumUnique(response string) int {
 	return len(seen)
 }
 
-// ReadFile returns a list responses per group
-func ReadFile(filename string) (responses []string) {
+// ReadFileIntoResponses returns a list responses per group. Each response represents one group.
+func ReadFileIntoResponses(filename string) (responses []string) {
 	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
@@ -47,6 +83,32 @@ func ReadFile(filename string) (responses []string) {
 	}
 	responses = append(responses, response)
 
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return
+}
+
+// ReadFileIntoGroups returns a list of groups. Each group is a list of
+// responses (one per person).
+func ReadFileIntoGroups(filename string) (groupResponses [][]string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(file)
+	groupResponses = [][]string{}
+	response := []string{}
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text == "" {
+			groupResponses = append(groupResponses, response)
+			response = []string{}
+		} else {
+			response = append(response, text)
+		}
+	}
+	groupResponses = append(groupResponses, response)
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
--- 2020/day6/day6_test.go
+++ 2020/day6/day6_test.go
@@ -5,19 +5,6 @@ import (
 	"testing"
 )
 
-func TestReadFile(t *testing.T) {
-	expected := []string{
-		"abc",
-		"abc",
-		"abac",
-		"aaaa",
-		"b"}
-	result := ReadFile("./input_test.txt")
-	if !reflect.DeepEqual(result, expected) {
-		t.Errorf("ReadFile failed. Received %v expected %v", result, expected)
-	}
-}
-
 func TestGetNumUnique1(t *testing.T) {
 	testGetNumUnique(t, "abc", 3)
 }
@@ -31,10 +18,58 @@ func TestGetNumUnique4(t *testing.T) {
 	testGetNumUnique(t, "b", 1)
 }
 
+func TestGetNumCommon1(t *testing.T) {
+	testGetNumCommon(t, []string{"abc"}, 3)
+}
+func TestGetNumCommon2(t *testing.T) {
+	testGetNumCommon(t, []string{"a", "b", "c"}, 0)
+}
+func TestGetNumCommon3(t *testing.T) {
+	testGetNumCommon(t, []string{"ab", "ac"}, 1)
+}
+func TestGetNumCommon4(t *testing.T) {
+	testGetNumCommon(t, []string{"a", "a", "a", "a"}, 1)
+}
+func TestGetNumCommon5(t *testing.T) {
+	testGetNumCommon(t, []string{"b"}, 1)
+}
+func TestReadFileIntoResponses(t *testing.T) {
+	expected := []string{
+		"abc",
+		"abc",
+		"abac",
+		"aaaa",
+		"b"}
+	result := ReadFileIntoResponses("./input_test.txt")
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("ReadFile failed. Received %v expected %v", result, expected)
+	}
+}
+func TestReadFileIntoGroups(t *testing.T) {
+	expected := [][]string{
+		{"abc"},
+		{"a", "b", "c"},
+		{"ab", "ac"},
+		{"a", "a", "a", "a"},
+		{"b"},
+	}
+	result := ReadFileIntoGroups("./input_test.txt")
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("ReadFile failed. Received %v expected %v", result, expected)
+	}
+}
+
 func testGetNumUnique(t *testing.T, response string, expected int) {
 	result := GetNumUnique(response)
 
 	if result != expected {
 		t.Errorf("GetNumUnique failed. Received %v expected %v", result, expected)
+	}
+}
+func testGetNumCommon(t *testing.T, group []string, expected int) {
+	result := GetNumCommon(group)
+
+	if result != expected {
+		t.Errorf("GetNumCommon failed. Received %v expected %v", result, expected)
 	}
 }
