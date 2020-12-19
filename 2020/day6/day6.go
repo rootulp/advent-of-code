49ad4cbaceb1922c52e70f5f01cfe6c38ae146b0
Rootul Patel <rootulp@amazon.com>
2020-12-18 20:51:45 -0500

    Solve day6 part1

--- 2020/day6/day6.go
+++ 2020/day6/day6.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Starting day6")
+	responses := ReadFile("input.txt")
+
+	sum := 0
+	for _, response := range responses {
+		unique := GetNumUnique(response)
+		sum += unique
+	}
+	fmt.Printf("Sum of unique counts %v\n", sum)
+}
+
+// GetNumUnique returns the number of unique characters in the provided response
+func GetNumUnique(response string) int {
+	seen := make(map[rune]bool)
+	for _, char := range response {
+		seen[char] = true
+	}
+	return len(seen)
+}
+
+// ReadFile returns a list responses per group
+func ReadFile(filename string) (responses []string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(file)
+	responses = []string{}
+	response := ""
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text == "" {
+			responses = append(responses, response)
+			response = ""
+		} else {
+			response += text
+		}
+	}
+	responses = append(responses, response)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return
 }
--- 2020/day6/day6_test.go
+++ 2020/day6/day6_test.go
@@ -1,7 +1,40 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func Test(t *testing.T) {
+func TestReadFile(t *testing.T) {
+	expected := []string{
+		"abc",
+		"abc",
+		"abac",
+		"aaaa",
+		"b"}
+	result := ReadFile("./input_test.txt")
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("ReadFile failed. Received %v expected %v", result, expected)
+	}
+}
 
+func TestGetNumUnique1(t *testing.T) {
+	testGetNumUnique(t, "abc", 3)
+}
+func TestGetNumUnique2(t *testing.T) {
+	testGetNumUnique(t, "abac", 3)
+}
+func TestGetNumUnique3(t *testing.T) {
+	testGetNumUnique(t, "aaaa", 1)
+}
+func TestGetNumUnique4(t *testing.T) {
+	testGetNumUnique(t, "b", 1)
+}
+
+func testGetNumUnique(t *testing.T, response string, expected int) {
+	result := GetNumUnique(response)
+
+	if result != expected {
+		t.Errorf("GetNumUnique failed. Received %v expected %v", result, expected)
+	}
 }
