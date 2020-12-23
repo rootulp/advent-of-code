9e876cb89b5465ba36ae5174a6ed24e31ee940ea
Rootul Patel <rootulp@amazon.com>
2020-12-22 19:12:53 -0500

    Add test case for input.txt

--- 2020/day8/day8_test.go
+++ 2020/day8/day8_test.go
@@ -3,9 +3,17 @@ package main
 import "testing"
 
 func TestGetAccumulatorValuePriorToFirstRepeatedInstruction(t *testing.T) {
-	t.Run("test_input", func(t *testing.T) {
+	t.Run("input_test", func(t *testing.T) {
 		got := GetAccumulatorValuePriorToFirstRepeatedInstruction("input_test.txt")
 		expected := 5
+
+		if got != expected {
+			t.Errorf("GetAccumulatorValuePriorToFirstRepeatedInstruction got %d expected %d", got, expected)
+		}
+	})
+	t.Run("input", func(t *testing.T) {
+		got := GetAccumulatorValuePriorToFirstRepeatedInstruction("input.txt")
+		expected := 1684
 
 		if got != expected {
 			t.Errorf("GetAccumulatorValuePriorToFirstRepeatedInstruction got %d expected %d", got, expected)
