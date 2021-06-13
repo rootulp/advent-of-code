c274921f0da100000c89f45631590e4cf9568904
Rootul Patel <rootulp@amazon.com>
2021-06-12 20:34:36 -0400

    Solve part two

--- 2020/day12/day12_test.go
+++ 2020/day12/day12_test.go
@@ -31,4 +31,13 @@ func TestGetManhattanDistance(t *testing.T) {
 			t.Errorf("GetManhattanDistancePartTwo input_test incorrect got %v want %v", got, want)
 		}
 	})
+
+	t.Run("GetManhattanDistancePartTwo input_test", func(t *testing.T) {
+		got := GetManhattanDistancePartTwo("input_test.txt")
+		want := 20592
+
+		if got != want {
+			t.Errorf("GetManhattanDistancePartTwo input_test incorrect got %v want %v", got, want)
+		}
+	})
 }
