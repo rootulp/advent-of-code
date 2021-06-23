a5cb9fc905622f33753fa0d20b7650ec3be88553
Rootul Patel <rootulp@amazon.com>
2021-06-22 21:14:22 -0400

    Write values to memory

--- 2020/14/day14.go
+++ 2020/14/day14.go
@@ -19,10 +19,8 @@ type instruction struct {
 	value     int
 }
 
-type memory map[int]int
-
 func main() {
-	fmt.Println("Startin day 14")
+	fmt.Println("Starting day 14")
 
 	// Part one
 	SumOfBitmaskedValues("example.txt")
@@ -30,7 +28,7 @@ func main() {
 
 func SumOfBitmaskedValues(filename string) (sum int) {
 	lines := readLines(filename)
-	var memory memory
+	memory := make(map[int]int)
 	var mask string
 	for _, line := range lines {
 		mask, memory = execute(line, mask, memory)
@@ -38,34 +36,45 @@ func SumOfBitmaskedValues(filename string) (sum int) {
 	return sumOfValues(memory)
 }
 
-func execute(line string, mask string, memory memory) (newMask string, newMemory memory) {
+func execute(line string, mask string, memory map[int]int) (string, map[int]int) {
 	instruction := parse(line)
-	fmt.Printf("operation %v address %v value %v bitmask %v\n", instruction.operation, instruction.address, instruction.value, instruction.bitmask)
-	return newMask, newMemory
+	switch operation := instruction.operation; operation {
+	case "mem":
+		fmt.Printf("attempting to set address: %v, value: %v\n", instruction.address, instruction.value)
+		memory[instruction.address] = instruction.value
+	case "mask":
+		mask = instruction.bitmask
+	default:
+		log.Fatalf("operation %v is not supported\n", operation)
+	}
+	fmt.Printf("mask: %v, memory %v\n", mask, memory)
+	return mask, memory
 }
 
-func parse(line string) instruction {
+func parse(line string) (i instruction) {
 	regex := regexp.MustCompile(`(?P<command>\w*)(\[(?P<address>\d*)\])?\s=\s(?P<value>\w*)`)
 	match := regex.FindStringSubmatch(line)
 	switch operation := match[1]; operation {
 	case mem:
 		address, _ := strconv.Atoi(match[3])
 		value, _ := strconv.Atoi(match[4])
-		return instruction{
+		i = instruction{
 			operation: operation,
 			address:   address,
 			value:     value,
 		}
 	case mask:
 		bitmask := match[4]
-		return instruction{
+		i = instruction{
 			operation: operation,
 			bitmask:   bitmask,
 		}
 	default:
-		log.Fatalf("Operation %v is not a valid operation: mem or mask", operation)
-		return instruction{}
+		log.Fatalf("operation %v is not supported\n", operation)
+		i = instruction{}
 	}
+	fmt.Printf("operation: %v, address: %v, value: %v, bitmask: %v\n", i.operation, i.address, i.value, i.bitmask)
+	return i
 }
 
 func sumOfValues(state map[int]int) (result int) {
