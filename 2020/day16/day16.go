fb1815720506eb96aaf7060e11d911ce009df3d0
Rootul Patel <rootulp@amazon.com>
2021-07-05 21:25:38 -0400

    Pass example

--- 2020/day16/day16.go
+++ 2020/day16/day16.go
@@ -5,10 +5,18 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 const YOUR_TICKET = "your ticket:"
 const NEARBY_TICKETS = "nearby tickets:"
+
+type Rule struct {
+	category string
+	rangeA   string
+	rangeB   string
+}
 
 func main() {
 	fmt.Println("Starting day16")
@@ -20,9 +28,78 @@ func main() {
 
 func TicketScanningErrorRate(filename string) (errorRate int) {
 	lines := readFile(filename)
-	rules, myTicket, nearbyTickets := split(lines)
+	rules, _, nearbyTickets := split(lines)
+	validNumbers := getValidNumbers(rules)
+	for _, ticket := range nearbyTickets {
+		errorRate += getErrorRate(ticket, validNumbers)
+	}
+	// fmt.Printf("validNumbers %v\n", validNumbers)
+	// fmt.Printf("nearbyTickets %v\n", nearbyTickets)
+	return errorRate
+}
 
-	return 0
+func getErrorRate(ticket string, validNumbers map[int]bool) (errorRate int) {
+	ticketNumbers := getNumbers(ticket)
+	for _, num := range ticketNumbers {
+		if validNumbers[num] {
+			continue
+		} else {
+			errorRate += num
+		}
+	}
+	return errorRate
+}
+
+func getValidNumbers(rules []string) (validNumbers map[int]bool) {
+	validNumbers = map[int]bool{}
+
+	for _, rule := range rules {
+		parsed := parseRule(rule)
+		// fmt.Printf("Parsed %+v\n", parsed)
+		startA, endA := parseRange(parsed.rangeA)
+		startB, endB := parseRange(parsed.rangeB)
+		for i := startA; i <= endA; i++ {
+			validNumbers[i] = true
+		}
+		for i := startB; i <= endB; i++ {
+			validNumbers[i] = true
+		}
+	}
+	return validNumbers
+}
+
+func getNumbers(ticket string) (numbers []int) {
+	strs := strings.Split(ticket, ",")
+	for _, s := range strs {
+		number, err := strconv.Atoi(s)
+		if err != nil {
+			log.Fatal(err)
+		}
+		numbers = append(numbers, number)
+	}
+	return numbers
+}
+
+func parseRule(rule string) (parsed Rule) {
+	s := strings.Split(rule, ":")
+	r := strings.Split(s[1], "or")
+	parsed.category = strings.TrimSpace(s[0])
+	parsed.rangeA = strings.TrimSpace(r[0])
+	parsed.rangeB = strings.TrimSpace(r[1])
+	return parsed
+}
+
+func parseRange(r string) (start int, end int) {
+	s := strings.Split(r, "-")
+	start, err := strconv.Atoi(s[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	end, err = strconv.Atoi(s[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	return start, end
 }
 
 // split lines into rules, myTicket, and nearbyTickets
@@ -33,14 +110,11 @@ func split(lines []string) (rules []string, myTicket string, nearbyTickets []str
 	for _, line := range lines {
 		if line == YOUR_TICKET {
 			seenYourTicket = true
-		}
-		if line == NEARBY_TICKETS {
+		} else if line == NEARBY_TICKETS {
 			seenNearbyTickets = true
-		}
-		if line == "" {
+		} else if line == "" {
 			continue
-		}
-		if seenNearbyTickets && seenYourTicket {
+		} else if seenNearbyTickets && seenYourTicket {
 			nearbyTickets = append(nearbyTickets, line)
 		} else if seenYourTicket {
 			myTicket = line
