617e41e5a839d5eec8a52408e48f13d10b374566
Rootul Patel <rootulp@amazon.com>
2021-07-06 21:10:57 -0400

    Unit test part two

--- 2020/day16/day16_test.go
+++ 2020/day16/day16_test.go
@@ -19,3 +19,12 @@ func TestPartOne(t *testing.T) {
 		t.Errorf("Incorrect TicketScanningErrorRate got %v want %v", got, want)
 	}
 }
+
+func TestPartTwo(t *testing.T) {
+	got := ProductOfDepartureValues("input.txt")
+	want := 362974212989
+
+	if got != want {
+		t.Errorf("Incorrect ProductOfDepartureValues got %v want %v", got, want)
+	}
+}
