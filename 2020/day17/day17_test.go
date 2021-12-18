2699fa62cf020f9214f62e08b6cb1b796e15982b
Rootul Patel <rootulp@gmail.com>
2021-12-18 18:30:58 -0500

    Test initial state

--- 2020/day17/day17_test.go
+++ 2020/day17/day17_test.go
@@ -1,1 +1,28 @@
 package main
+
+import "testing"
+
+func TestPartOneExampleInitialState(t *testing.T) {
+	lines := readFile("example.txt")
+	got := newState(lines)
+	want := state{[gridSize][gridSize][gridSize]rune{
+		{ // z = -1
+			{'.', '.', '.'},
+			{'.', '.', '.'},
+			{'.', '.', '.'},
+		},
+		{ // z = 0
+			{'.', '#', '.'},
+			{'.', '.', '#'},
+			{'#', '#', '#'},
+		},
+		{ // z = 1
+			{'.', '.', '.'},
+			{'.', '.', '.'},
+			{'.', '.', '.'},
+		},
+	}}
+	if got != want {
+		t.Errorf("Incorrect example initial state got %v want %v", got, want)
+	}
+}
