29463d646eee6fd319222bea122990cf78027285
Rootul Patel <rootulp@gmail.com>
2022-01-25 22:19:35 -0500

    Pass first ReversePolishNotation test

--- 2020/day18/day18.go
+++ 2020/day18/day18.go
@@ -4,7 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
+
+var operatorPrecedence map[string]int = map[string]int{
+	"+": 1,
+	"*": 1,
+}
 
 func main() {
 	fmt.Println("Starting day18")
@@ -28,8 +35,62 @@ func PartOne(filename string) (sum int, err error) {
 	return sum, nil
 }
 
+// ReversePolishNotation converts expression to Reverse Polish Notation using
+// the Shunting-yard algorithm
+// https://en.wikipedia.org/wiki/Shunting-yard_algorithm
 func ReversePolishNotation(expression string) (result string) {
-	return expression
+	output := []string{}
+	operatorStack := []string{}
+	tokens := strings.Split(expression, " ")
+	for _, token := range tokens {
+		switch {
+		case isNumber(token):
+			output = append(output, token)
+		case isOperator(token):
+			if len(operatorStack) > 0 {
+				for top := operatorStack[len(operatorStack)]; top != "(" && operatorPrecedence[top] >= operatorPrecedence[token]; {
+					output = append(output, token)
+					operatorStack = operatorStack[:len(operatorStack)-1]
+				}
+			}
+			operatorStack = append(operatorStack, token)
+		case token == "(":
+			operatorStack = append(operatorStack, token)
+		case token == ")":
+			for top := operatorStack[len(operatorStack)]; top != "("; {
+				if len(operatorStack) == 0 {
+					panic("operatorStack empty but expected more tokens")
+				}
+				output = append(output, top)
+			}
+			// Discard the left parenthesis at the top of the stack
+			leftParenthesis := operatorStack[len(operatorStack)-1]
+			operatorStack = operatorStack[:len(operatorStack)-1]
+			if leftParenthesis != "(" {
+				panic(fmt.Sprintf("expected %v to be left parenthesis", leftParenthesis))
+			}
+		}
+	}
+	for len(operatorStack) != 0 {
+		top := operatorStack[len(operatorStack)-1]
+		operatorStack = operatorStack[:len(operatorStack)-1]
+		if top == "(" {
+			panic(fmt.Sprintf("expected %v to not be left parenthesis", top))
+		}
+		output = append(output, top)
+	}
+	fmt.Printf("operatorStack %v\n", operatorStack)
+	fmt.Printf("output %v\n", output)
+	return strings.Join(output, " ")
+}
+
+func isNumber(s string) bool {
+	_, err := strconv.Atoi(s)
+	return err == nil
+}
+
+func isOperator(s string) bool {
+	return s == "+" || s == "*" || s == "(" || s == ")"
 }
 
 func Evaluate(expression string) (result int) {
