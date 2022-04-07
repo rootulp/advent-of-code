6cbbb16303d7bcb2a94e7ab4405133bc457af884
Rootul Patel <rootulp@gmail.com>
2022-04-07 18:38:56 -0400

    Add neighbors to tiles

--- 2020/day20/day20.go
+++ 2020/day20/day20.go
@@ -9,9 +9,25 @@ import (
 	"strings"
 )
 
+type Point struct {
+	row int
+	col int
+}
+
 type Tile struct {
-	id       int
-	contents []string
+	id        int
+	contents  []string
+	neighbors map[int]string // map from tile id to side
+}
+
+func (t Tile) String() string {
+	return fmt.Sprintf("Tile: %v\nContents:\n%v\nNeighbors: %v\n\n", t.id, strings.Join(t.contents, "\n"), t.neighbors)
+}
+
+func (t Tile) getAllSides() (allSides []string) {
+	allSides = append(allSides, t.getSides()...)
+	allSides = append(allSides, t.getSidesReversed()...)
+	return allSides
 }
 
 func (t Tile) getSides() []string {
@@ -30,23 +46,23 @@ func (t Tile) getSide(side string) string {
 	case "bottom":
 		return t.contents[len(t.contents)-1]
 	case "right":
-		return t.rightBorder()
+		return t.getRightSide()
 	case "left":
-		return t.leftBorder()
+		return t.getLeftSide()
 	default:
 		log.Fatalf("unrecognized side %v", side)
 	}
 	return ""
 }
 
-func (t Tile) reversedBorders() (reversed []string) {
+func (t Tile) getSidesReversed() (reversed []string) {
 	for _, border := range t.getSides() {
 		reversed = append(reversed, reverse(border))
 	}
 	return reversed
 }
 
-func (t Tile) leftBorder() (result string) {
+func (t Tile) getLeftSide() (result string) {
 	for _, row := range t.contents {
 		vals := strings.Split(row, "")
 		result += vals[0]
@@ -54,7 +70,7 @@ func (t Tile) leftBorder() (result string) {
 	return result
 }
 
-func (t Tile) rightBorder() (result string) {
+func (t Tile) getRightSide() (result string) {
 	for _, row := range t.contents {
 		vals := strings.Split(row, "")
 		result += vals[len(vals)-1]
@@ -98,15 +114,15 @@ func PartOne(filename string) (productOfCornerIds int) {
 	lines := readLines(filename)
 	tiles := parseTiles(lines)
 
-	fmt.Printf("tiles: %v\n", tiles)
-	fmt.Printf("len(tiles): %v\n", len(tiles))
-	fmt.Printf("borders for first tile: %v\n", tiles[0].getSides())
+	// fmt.Printf("tiles: %v\n", tiles)
+	// fmt.Printf("len(tiles): %v\n", len(tiles))
+	// fmt.Printf("borders for first tile: %v\n", tiles[0].getSides())
 
 	occurences := countBorderOccurences(tiles)
-	fmt.Printf("occurences %v\n", occurences)
+	// fmt.Printf("occurences %v\n", occurences)
 
 	cornerTileIds := cornerTiles(tiles, occurences)
-	fmt.Printf("cornerTileIds %v\n", cornerTileIds)
+	// fmt.Printf("cornerTileIds %v\n", cornerTileIds)
 
 	productOfCornerIds = 1
 	for _, cornerTileId := range cornerTileIds {
@@ -117,8 +133,15 @@ func PartOne(filename string) (productOfCornerIds int) {
 }
 
 func PartTwo(filename string) (numberOfPoundSignsNotPartOfSeaMonsters int) {
-	// lines := readLines(filename)
-	// tiles := parseTiles(lines)
+	lines := readLines(filename)
+	tiles := parseTiles(lines)
+	fmt.Printf("tiles %v\n", tiles)
+	// tilesMap := getTilesMap(tiles)
+	// occurences := countBorderOccurences(tiles)
+	// cornerTileIds := cornerTiles(tiles, occurences)
+
+	// pic := assemble(tilesMap, cornerTileIds)
+	// fmt.Printf("pic\n%v\n", pic)
 
 	return numberOfPoundSignsNotPartOfSeaMonsters
 }
@@ -132,7 +155,7 @@ func cornerTiles(tiles []Tile, occurences map[string]int) (cornerTileIds []int) 
 		for _, border := range borders {
 			numSharedBorders += (occurences[border])
 		}
-		reversedBorders := tile.reversedBorders()
+		reversedBorders := tile.getSidesReversed()
 		for _, reversedBorder := range reversedBorders {
 			numSharedBorders += (occurences[reversedBorder])
 		}
@@ -145,7 +168,7 @@ func cornerTiles(tiles []Tile, occurences map[string]int) (cornerTileIds []int) 
 			cornerTileIds = append(cornerTileIds, tile.id)
 		}
 	}
-	fmt.Printf("tileToSharedBorders %v\n", tileToSharedBorders)
+	// fmt.Printf("tileToSharedBorders %v\n", tileToSharedBorders)
 	return cornerTileIds
 }
 
@@ -156,7 +179,7 @@ func countBorderOccurences(tiles []Tile) (occurences map[string]int) {
 		for _, border := range borders {
 			occurences[border] += 1
 		}
-		reversedBorders := tile.reversedBorders()
+		reversedBorders := tile.getSidesReversed()
 		for _, reversedBorder := range reversedBorders {
 			occurences[reversedBorder] += 1
 		}
@@ -174,7 +197,7 @@ func parseTiles(lines []string) (tiles []Tile) {
 			if err != nil {
 				log.Fatal(err)
 			}
-			tile = Tile{id: id}
+			tile = Tile{id, []string{}, map[int]string{}}
 		} else if strings.HasPrefix(line, ".") || strings.HasPrefix(line, "#") {
 			tile.contents = append(tile.contents, line)
 		} else {
@@ -182,7 +205,31 @@ func parseTiles(lines []string) (tiles []Tile) {
 		}
 	}
 	tiles = append(tiles, tile)
+
+	for a, tileA := range tiles {
+		for b, tileB := range tiles {
+			if a == b {
+				continue
+			}
+			sharedSides := getSharedSides(tileA.getAllSides(), tileB.getAllSides())
+			for _, side := range sharedSides {
+				tileA.neighbors[tileB.id] = side
+				tileB.neighbors[tileA.id] = side
+			}
+		}
+	}
 	return tiles
+}
+
+func getSharedSides(sidesA []string, sidesB []string) (shared []string) {
+	for _, a := range sidesA {
+		for _, b := range sidesB {
+			if a == b {
+				shared = append(shared, a)
+			}
+		}
+	}
+	return shared
 }
 
 func readLines(filename string) (lines []string) {
@@ -199,6 +246,56 @@ func readLines(filename string) (lines []string) {
 	}
 
 	return lines
+}
+
+func assemble(tilesMap map[int]Tile, cornerTileIds []int) (fullpic []string) {
+	var r = 0
+	var c = 0
+	var gridmap map[Point]int
+	// var fullpic = []string{}
+
+	for len(gridmap) < len(tilesMap) {
+		if r == 0 && c == 0 {
+			cornerTileId := cornerTileIds[0]
+			cornerTile := tilesMap[cornerTileId]
+			fmt.Printf("cornerTile %v\n", cornerTile)
+
+			// neighbors = list(tiles[idn]["neighbors"].values())
+			// neighbors += [n[::-1] for n in neighbors]
+			// while True:
+			// 	bottom, right = get_side(tile, "bottom"), get_side(tile, "right")
+			// 	if bottom in neighbors and right in neighbors:
+			// 		break
+			// 	tile = rotate(tile)
+			// gridmap[(r, c)] = idn
+			// tiles[idn]["grid"] = tile
+			// add_fullpic(tile)
+
+			c += 1
+		} else if c == 0 {
+			// 	# add new tile based on first tile in previous row's bottom
+			// 	...[snip]...
+			c += 1
+		} else {
+			// 	# look for next in row
+			// 	...[snip]...
+			// 	if find next:
+			// 		# add to row
+			// 		...[snip]...
+			// 		c += 1
+			// 	else:
+			// 		r, c = r + 1, 0
+		}
+	}
+	return fullpic
+}
+
+func getTilesMap(tiles []Tile) (result map[int]Tile) {
+	result = map[int]Tile{}
+	for _, tile := range tiles {
+		result[tile.id] = tile
+	}
+	return result
 }
 
 func addTileToPic(pic []string, tile Tile, newline bool) (newPic []string) {
