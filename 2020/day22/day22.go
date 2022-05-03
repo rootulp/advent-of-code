9b05ca30fbcb569565990446a201f4f4768cb231
Rootul Patel <rootulp@gmail.com>
2022-05-03 15:38:21 -0400

    Read in lines to deck

--- 2020/day22/day22.go
+++ 2020/day22/day22.go
@@ -1,6 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Deck struct {
+	name string
+	cards []int
+}
+
+func NewDeck(name string, lines []string) Deck {
+	cards := []int{}
+	for _, line := range lines {
+		if strings.HasPrefix(line, "Player") {
+			continue
+		}
+		card, err := strconv.Atoi(line)
+		if err != nil {
+			log.Fatal(err)
+		}
+		cards = append(cards, card)
+	}
+	return Deck{name, cards}
+}
+
+func (d Deck) String() string {
+	return fmt.Sprintf("%s: %v", d.name, d.cards)
+}
 
 func main() {
 	fmt.Printf("Starting day22...\n")
@@ -13,5 +44,34 @@ func main() {
 }
 
 func PartOne(filename string) (score int) {
+	lines := readLines(filename)
+
+	playerOneLines := lines[:len(lines) / 2]
+	playerTwoLines := lines[len(lines) / 2 + 1:]
+
+	deckOne := NewDeck("Player 1", playerOneLines)
+	deckTwo := NewDeck("Player 2", playerTwoLines)
+
+	fmt.Println(deckOne)
+	fmt.Println(deckTwo)
 	return 0
+}
+
+func readLines(filename string) (lines []string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return lines
 }
